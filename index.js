efe72d4f27232e48d89003b4663d4618c2e46037
brucemorakane <brucemorakane74@gmail.com.com>
2022-07-05 14:16:45 +0200

    added index.js changed

---
+++ index.js
@@ -0,0 +1,10 @@
+var state ={
+    expense:200,
+    income:1000,
+    balance:6000,
+item[
+    {item:'salary',type:'income', amount:'R6000'}
+    {item:'electrcity',type:'Expense',amount:'R750'}
+    {item:'insurance',type:'expense',amount:'R250'}
+]
+}
